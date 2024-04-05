5d75dfa26e589574c49ebb4b2a6daf52cc936836
Ayush Kumar <40995741+vancityAyush@users.noreply.github.com>
2024-04-05 11:09:05 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
-cd $USERPROFILE
+
+cd $HOME
 mkdir -p .ssh
 cd .ssh
-
-# Step 2: Show a menu to ask for option 1. Bitbucket and 2. Github
-
 
 echo "Select an option:"
 echo "1. Bitbucket"
 echo "2. Github"
+echo "3. GitLab"
 read -p "Enter your choice: " option
 
-# Step 3: Ask for email and ssh key name from the user
-read -p "Enter your email: " email
-read -p "Enter your SSH key name: " keyName
+echo "Enter your email: "
+read email
+echo "Enter your SSH key name: "
+read keyName
 
-# Step 5: Generate SSH key based on the selected option
 if [ "$option" = "1" ]; then
   ssh-keygen -t ed25519 -b 4096 -C "$email" -f "$keyName"
 elif [ "$option" = "2" ]; then
   ssh-keygen -t ed25519 -C "$email" -f "$keyName"
+elif [ "$option" = "3" ]; then
+  ssh-keygen -t rsa -b 4096 -C "$email" -f "$keyName"
 fi
 
-# Step 6: Add the generated SSH key to the ssh agent
 eval $(ssh-agent)
 ssh-add "$keyName"
 
-# Step 7: Create/update ssh configuration file
 config_file="config"
 touch "$config_file"
 
-#function to get host name
 getDefaultHostName() {
   if [ "$option" = "1" ]; then
     echo "bitbucket.org"
   elif [ "$option" = "2" ]; then
     echo "github.com"
+  elif [ "$option" = "3" ]; then
+    echo "gitlab.com"
   fi
 }
 
-#function to write config
 writeConfig(){
   if ! grep -q "Host $1" "$config_file"; then
     echo -e "\nHost $1" >> "$config_file"
@@ -52,9 +51,7 @@ writeConfig(){
   fi
 }
 
-
 read -p "Do you want to add custom host name? (Y/N): " choiceHost
-
 
 if [[ "$choiceHost" == [Yy] ]]; then
     read -p "Enter your host name like (work.github.com): " hostName
@@ -64,11 +61,10 @@ fi
 
 writeConfig $hostName $keyName
 
-# Step 8: Copy the keyName.pub file to the clipboard and echo it
 copy_to_clipboard() {
-  if command -v xclip &> /dev/null; then  # Check for xclip (Linux)
+  if command -v xclip &> /dev/null; then
     xclip -selection clipboard < "$1"
-  elif command -v clip.exe &> /dev/null; then  # Check for clip.exe (Windows)
+  elif command -v clip.exe &> /dev/null; then
     clip.exe < "$1"
   else
     echo "Warning: Neither xclip (Linux) nor clip.exe (Windows) found. Cannot copy to clipboard."
@@ -79,19 +75,20 @@ copy_to_clipboard "$keyName.pub"
 echo "SSH Public Key copied to clipboard:"
 cat "$keyName.pub"
 
-# Step 9: Open GitHub SSH Key Settings in the browser
 open_settings_page() {
-  if [ "$1" = "1" ]; then  # Bitbucket
+  if [ "$1" = "1" ]; then
     settings_url="https://bitbucket.org/account/settings/ssh-keys/"
-  elif [ "$1" = "2" ]; then  # GitHub
+  elif [ "$1" = "2" ]; then
     settings_url="https://github.com/settings/keys"
+  elif [ "$1" = "3" ]; then
+    settings_url="https://gitlab.com/-/profile/keys"
   fi
 
-  if command -v xdg-open &> /dev/null; then   # Linux 
+  if command -v xdg-open &> /dev/null; then
     xdg-open "$settings_url"
-  elif command -v open &> /dev/null; then     # macOS
+  elif command -v open &> /dev/null; then
     open "$settings_url"
-  elif command -v start &> /dev/null; then   # Windows
+  elif command -v start &> /dev/null; then
     start "$settings_url"
   else
     echo "Could not detect a command to open URLs in the browser."
@@ -100,15 +97,11 @@ open_settings_page() {
 
 open_settings_page "$option"
 
-
 while true; do
-  # Step 9: Wait for the user to press T for Test
   read -p "Press T to test SSH key or any other key to exit: " choice
   echo
 
-
   if [ "$choice" = "T" ] || [ "$choice" = "t" ]; then
-    # Test the SSH key based on the selected option
     ssh -T git@"$hostName"
   else
     break
